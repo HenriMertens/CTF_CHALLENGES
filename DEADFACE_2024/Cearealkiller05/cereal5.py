797807c18a6d06c64d0a01de23c0363124106308
HenriMertens <149707229+HenriMertens@users.noreply.github.com>
2024-10-20 12:13:23 +0200

    Update cereal5.py

--- DEADFACE_2024/Cearealkiller05/cereal5.py
+++ DEADFACE_2024/Cearealkiller05/cereal5.py
@@ -1,4 +1,6 @@
 from string import printable
+
+#extracted from java
 encryptedurl =[
             42, 6, 68, 64, 7, 120, 93, 31, 83, 17,
             48, 23, 81, 92, 90, 46, 11, 68, 68, 27,
@@ -7,23 +9,26 @@ encryptedurl =[
             45, 29, 68, 82, 6, 45, 29, 68, 30, 30,
             50, 23, 87]
 
+#we know url has this prefix
 url = "https://"
 
+#ascii value of url
 url_bytes = [104, 116, 116, 112, 115, 58, 47, 47]
 
 
 passw = ""
+
+#xoring the bytes
 for i in range(len(url)):
     for c in printable:
         if(encryptedurl[i] ^ ord(c) == url_bytes[i]):
             passw += c
             break
 
+#making sure it happend correct, we should see "https" being printed"
 testpass = ""
 def test(passw, j):
     return chr(ord(passw) ^ encryptedurl[j])
-
-
 
 for i in range(len(passw)):
     testpass += test(passw[i], i)
@@ -32,4 +37,5 @@ for i in range(len(passw)):
 
 print(passw)
 print(testpass)
+#print the length
 print(len(encryptedurl))
